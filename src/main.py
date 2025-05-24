fba7e1fd871a39f4efbd25a72d5d3fb3416533aa
TheWorldJar <142929823+TheWorldJar@users.noreply.github.com>
2025-05-24 16:11:18 -0400

    Slight modification to dummy main.

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ from htmlnode import HTMLNode, LeafNode, ParentNode
 def main():
     dummy = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print (dummy)
+    dummy_html = dummy.to_html_node()
+    print (dummy_html)
 
 if __name__ == "__main__":
     main()
