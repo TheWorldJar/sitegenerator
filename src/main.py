485a9502607115a07d82d5542c0b1e1a09f47cfd
TheWorldJar <142929823+TheWorldJar@users.noreply.github.com>
2025-06-05 10:45:43 -0400

    Enhance main script and page generation logic to accept a base path argument for improved URL handling.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
+import sys
 from filemanip import copy_static_to_public
 from page import find_pages
 
 
 def main():
-    # md = "1. This\n2. _Is_\n3. A **bold**\n4. Quote\n\nA **bold** choice of _words_!\n\n- ![image1](https://fake.adress.com/image1.png)\n\n```\nThis is a code block\n```\n\n>This\n>Is\n>A\n>Proper\n>Quote"
-    # print(markdown_to_html_node(md).to_html())
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
     copy_static_to_public()
-    find_pages("./content", "template.html", "./public")
+    find_pages("./content", "template.html", "./public", basepath)
 
 
 if __name__ == "__main__":
--- src/page.py
+++ src/page.py
@@ -4,7 +4,7 @@ from conversion import markdown_to_html_node
 from htmlnode import LeafNode
 
 
-def find_pages(content_path: str, template_path: str, dest_path: str):
+def find_pages(content_path: str, template_path: str, dest_path: str, basepath: str):
     for dir in os.listdir(content_path):
         full_path = os.path.join(content_path, dir)
         if os.path.isfile(full_path) and dir.endswith(".md"):
@@ -14,14 +14,15 @@ def find_pages(content_path: str, template_path: str, dest_path: str):
                 dest_path
                 + full_path.split(".md")[0].split("./content", 1)[1]
                 + ".html",
+                basepath,
             )
         elif os.path.isdir(full_path):
-            find_pages(full_path, template_path, dest_path)
+            find_pages(full_path, template_path, dest_path, basepath)
         else:
             raise Exception("Invalid file type found in the contents folder!")
 
 
-def generate_page(from_path: str, template_path: str, dest_path: str):
+def generate_page(from_path: str, template_path: str, dest_path: str, basepath: str):
     if os.path.isfile(from_path):
         print(f"Generating page from {from_path} to {dest_path} using {template_path}")
         with open(from_path) as file:
@@ -36,6 +37,8 @@ def generate_page(from_path: str, template_path: str, dest_path: str):
             html_title,
             re.sub(r"\{\{\sContent\s\}\}", html_body, read_template),
         )
+        full_html = re.sub(r"href=\"\/", 'href="' + basepath, full_html)
+        full_html = re.sub(r"src=\"\/", 'src="' + basepath, full_html)
         prepare_dest(dest_path)
         with open(dest_path, "w") as dest:
             dest.write(full_html)
